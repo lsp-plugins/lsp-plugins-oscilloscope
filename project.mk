a7d3dcab3e4d64b1bccc5631ba0425c9fcb65db7
Vladimir Sadovnikov <sadko4u@gmail.com>
2023-01-03 19:49:12 +0300

    Updated build scripts and dependencies

--- project.mk
+++ project.mk
@@ -24,6 +24,7 @@ ARTIFACT_NAME               = lsp-plugins-oscilloscope
 ARTIFACT_TYPE               = plug
 ARTIFACT_DESC               = LSP Oscilloscope Plugin Series
 ARTIFACT_HEADERS            = lsp-plug.in
+ARTIFACT_EXPORT_HEADERS     = 0
 ARTIFACT_VERSION            = 1.0.6-devel
 
 
