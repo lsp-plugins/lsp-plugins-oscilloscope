3c40d4d9dfd597732b3a48e36b6ac80d0210eebe
Vladimir Sadovnikov <sadko4u@gmail.com>
2023-03-19 15:23:12 +0300

    Updated build dependencies

--- src/Makefile.d
+++ src/Makefile.d
@@ -404,6 +404,7 @@ $(LSP_PLUGINS_OSCILLOSCOPE_BIN)/main/ui/oscilloscope.o: \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ui/IPlayListener.h \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ui/ControlPort.h \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ui/PathPort.h \
+ $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ui/ProxyPort.h \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ui/Factory.h \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ui/UIOverrides.h \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ui/UIContext.h \
